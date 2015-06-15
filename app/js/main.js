d63e5935cffa2c877c1d2e1dbc42858a059b81cb
petrozavodsky <petrozavodsky@gmail.com>
2015-06-16 00:08:07 +0300

    save

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,19 @@ $(document).ready(function () {
     $("[href='#request']").fancybox({
         type: 'ajax',
         padding: 54,
-        width:420,
+        width: 420,
+    });
+
+
+    $('.clients__items').carouFredSel({
+        prev: '.clients__slider .clients__slider-arrow-left',
+        next: '.clients__slider .clients__slider-arrow-right',
+        width: 900,
+        items: 4,
+        scroll:{
+            items:1,
+            fx: 'crossfade',
+        }
     });
 
 
