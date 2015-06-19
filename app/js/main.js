ee3cecae03667af08b9699039cb8a63aab96b7a6
petrozavodsky <petrozavodsky@gmail.com>
2015-06-20 00:08:57 +0300

    final commit

--- app/js/main.js
+++ app/js/main.js
@@ -14,30 +14,33 @@ $(document).ready(function () {
         items: 4,
         scroll: {
             items: 1,
-            fx: 'crossfade',
+            fx: 'crossfade'
         }
     });
 
-    if ($("#map").length) {
 
-        ymaps.ready(init);
-        function init() {
-            var myMap = new ymaps.Map("map", {
-                    center: [45.04813364, 38.97383567],
-                    zoom: 13,
-                    controls: ['zoomControl']
-                }),
-                myPlacemark = new ymaps.Placemark(myMap.getCenter(), {hintContent: 'Seomantika, продвижение сайтов'}, {
-                    iconLayout: 'default#image',
-                    iconImageHref: $("#map").attr('data-img'),
-                    iconImageSize: [88, 97],
-                    iconImageOffset: [-44, -97]
-                });
 
-            myMap.behaviors.disable(['scrollZoom']);
-            myMap.geoObjects.add(myPlacemark);
+    $(window).load(function () {
+        if ($("#map").length) {
+
+            function init() {
+                var myMap = new ymaps.Map("map", {
+                        center: [45.04813364, 38.97383567],
+                        zoom: 13,
+                        controls: ['zoomControl']
+                    }),
+                    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {hintContent: 'Seomantika, продвижение сайтов'}, {
+                        iconLayout: 'default#image',
+                        iconImageHref: $("#map").attr('data-img'),
+                        iconImageSize: [88, 97],
+                        iconImageOffset: [-44, -97]
+                    });
+
+                myMap.behaviors.disable(['scrollZoom']);
+                myMap.geoObjects.add(myPlacemark);
+            }
+
+            ymaps.ready(init);
         }
-
-    }
-
+    });
 });
