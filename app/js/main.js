1e4b47fbe16d36c6563ce56663a5b87c22af316c
petrozavodsky <petrozavodsky@gmail.com>
2015-06-19 22:53:27 +0300

    save

--- app/js/main.js
+++ app/js/main.js
@@ -12,11 +12,32 @@ $(document).ready(function () {
         next: '.clients__slider .clients__slider-arrow-right',
         width: '100%',
         items: 4,
-        scroll:{
-            items:1,
+        scroll: {
+            items: 1,
             fx: 'crossfade',
         }
     });
 
+    if ($("#map").length) {
+
+        ymaps.ready(init);
+        function init() {
+            var myMap = new ymaps.Map("map", {
+                    center: [45.04813364, 38.97383567],
+                    zoom: 13,
+                    controls: ['zoomControl']
+                }),
+                myPlacemark = new ymaps.Placemark(myMap.getCenter(), {hintContent: 'Seomantika, продвижение сайтов'}, {
+                    iconLayout: 'default#image',
+                    iconImageHref: $("#map").attr('data-img'),
+                    iconImageSize: [88, 97],
+                    iconImageOffset: [-44, -97]
+                });
+
+            myMap.behaviors.disable(['scrollZoom']);
+            myMap.geoObjects.add(myPlacemark);
+        }
+
+    }
 
 });
--- app/js/vendor.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by piston on 12.06.2015.
- */
